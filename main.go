2629278461729bd0e1035dfce9606d5105f1abd5
github user tmcoco0228si <proguteinitar3150@gmail.com>
2024-03-16 00:09:38 +0900

    feat: add some handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -14,9 +15,28 @@ func main() {
 	postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "post article...\n")
 	}
+	postListHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "postListHandler...\n")
+	}
+	postDetailHandler := func(w http.ResponseWriter, req *http.Request) {
+		articleID := 1
+		resString := fmt.Sprintf("article_%d", articleID)
+		io.WriteString(w, resString)
 
-	http.HandleFunc("/", helloHandler)
+	}
+	postNiceHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Nice...\n")
+	}
+	postCommentHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Comment...\n")
+	}
+
+	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/article", postArticleHandler)
+	http.HandleFunc("/article/list", postListHandler)
+	http.HandleFunc("/article/1", postDetailHandler)
+	http.HandleFunc("/article/nice", postNiceHandler)
+	http.HandleFunc("/comment", postCommentHandler)
 	log.Println("sever start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
