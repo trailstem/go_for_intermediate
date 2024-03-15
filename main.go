d60384ebce32633397e1fe7f9f9f3c03e0211613
github user tmcoco0228si <proguteinitar3150@gmail.com>
2024-03-16 00:04:23 +0900

    feat: add postArticleHandle

--- main.go
+++ main.go
@@ -10,8 +10,13 @@ func main() {
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "Hello, world!")
 	}
-	http.HandleFunc("/", helloHandler)
 
+	postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "post article...\n")
+	}
+
+	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/article", postArticleHandler)
 	log.Println("sever start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
